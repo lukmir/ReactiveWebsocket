79b6ed5e283af3b310224f5bd40ff076929bc88a
lukmir <lukmir2@gmail.com>
2018-04-10 18:54:39 +0200

    adding rest config

---
+++ src/main/java/com/application/controller/rest/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package com.application.controller.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class ApplicationConfig extends Application {
+}
