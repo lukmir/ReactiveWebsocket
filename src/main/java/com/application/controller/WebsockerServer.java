91d0b3c263fc1d976de64b53f9a109659cdd80d4
lukmir <lukmir2@gmail.com>
2018-04-10 18:48:05 +0200

    adding websocket servlet for support maintenance session list based on onerror, onclose, onopen and onmessage events

---
+++ src/main/java/com/application/controller/WebsockerServer.java
@@ -0,0 +1,38 @@
+package com.application.controller;
+
+
+import javax.ejb.Singleton;
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Singleton
+@ServerEndpoint(value = "/websocketServer")
+public class WebsockerServer {
+
+    private final List<Session> sessionList = Collections.synchronizedList(new ArrayList<>());
+
+    @OnOpen
+    public void onOpen(Session session) {
+        sessionList.add(session);
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        sessionList.remove(session);
+    }
+
+    @OnError
+    public void onError(Throwable error) {
+        System.err.println(error.getMessage());
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        sessionList.stream().filter( p -> p.isOpen()).forEachOrdered( p -> {
+            p.getAsyncRemote().sendText(message + " - Total sessions: " + sessionList.size());
+        });
+    }
+}
