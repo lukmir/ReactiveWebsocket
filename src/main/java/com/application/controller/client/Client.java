3131223c6e37c0529d4d015aba3db178d5dce26b
lukmir <lukmir2@gmail.com>
2018-04-10 18:53:30 +0200

    adding websocket client

---
+++ src/main/java/com/application/controller/client/Client.java
@@ -0,0 +1,49 @@
+package com.application.controller.client;
+
+import javax.ejb.Stateless;
+import javax.websocket.*;
+import javax.ws.rs.container.AsyncResponse;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@ClientEndpoint
+@Stateless
+public class Client {
+
+    private final String serverPath = "ws://localhost:8080/ReactiveWebsocket/websocketServer";
+
+    private Session session;
+    private final AsyncResponse asyncResponse;
+
+    public Client(AsyncResponse asyncResponse) {
+        this.asyncResponse = asyncResponse;
+    }
+
+    public void connect() {
+        WebSocketContainer webSocketContainer = ContainerProvider.getWebSocketContainer();
+        try {
+            webSocketContainer.connectToServer(this, new URI(serverPath));
+        } catch (URISyntaxException | DeploymentException | IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        asyncResponse.resume(message);
+    }
+
+    public void close() {
+        try {
+            session.close();
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+}
